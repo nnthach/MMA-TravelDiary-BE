e149ef04239165a3df6339a4618719f47573c73a
Harry <thcstrandainghiathinh6a1@gmail.com>
2025-05-28 08:31:05 +0700

    base db

---
+++ src/config/mongodb.js
@@ -0,0 +1,36 @@
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import {env} from '~/config/environment.js'
+
+
+let travelDiaryDatabaseInstance = null
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+})
+
+
+
+export const CONNECT_DB = async () => {
+  try {
+    await mongoClientInstance.connect()
+    travelDiaryDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+    console.log(' Connected to MongoDB successfully')
+  } catch (error) {
+    console.error(' Failed to connect to MongoDB:', error)
+    throw error
+  }
+}
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
+
+export const GET_DB = () => {
+  if (!travelDiaryDatabaseInstance) throw new Error('Must connect to Database first')
+  return travelDiaryDatabaseInstance
+}
+
--- src/routes/v1/travelRoutes.js
+++ src/routes/v1/travelRoutes.js
@@ -5,15 +5,16 @@ import {env} from '~/config/environment.js'// Hoặc dùng import dotenv from 'd
 import express from 'express'
 import exitHook from 'async-exit-hook'
 import { CONNECT_DB, GET_DB, CLOSE_DB } from './config/mongodb.js'  // dùng đường dẫn tương đối + .js
-import { APIs_V1 } from '~/routes/v1'
+import {APIs_V1} from '~/routes/v1'
 const hostname = 'localhost'
 const port = 3000
 
 const START_SERVER = () => {
   const app = express()
 
-  app.use('/v1', APIs_V1)
-  
+  app.get('/', async (req, res) => {
+   res.end('<h1> Hello World!</h1><hr>!')
+  })
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Hi ${env.AUTHOR }, Server running at http://${env.APP_HOST}:${env.APP_PORT}/`)
   })
